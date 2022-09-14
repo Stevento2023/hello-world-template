765f6777765b37b43c42d49f8c470931c76d113f
Steven To <steven.to@mrjaffesclass.com>
2022-09-14 11:43:33 -0700

    Wednesday Work

---
+++ helloworldtemplate.java
@@ -0,0 +1,13 @@
+public class helloworldtemplate
+{
+    public static void main(String[] args)
+    {
+        int i = 10;
+        int f = 0;
+        f = i+f;
+        boolean flag = false;
+        flag = false;
+        i += 1;
+        System.out.println("i,f");
+    }
+}
