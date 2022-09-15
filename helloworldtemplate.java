5a9a85cee57c0ed461df249380beda9a1a9a47aa
Steven To <steven.to@mrjaffesclass.com>
2022-09-15 11:09:02 -0700

    Operators in Java Complete Thursday

--- helloworldtemplate.java
+++ helloworldtemplate.java
@@ -5,9 +5,10 @@ public class helloworldtemplate
         int i = 10;
         int f = 0;
         f = i+f;
-        boolean flag = false;
-        flag = false;
+        boolean flag = true;
+        flag = !false;
         i += 1;
-        System.out.println("i,f");
+        System.out.println(i);
+        System.out.println(f);
     }
 }
