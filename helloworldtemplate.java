e28cda38e7f49c98133731da903a3c13d15fe2a4
Steven To <steven.to@mrjaffesclass.com>
2022-09-16 11:10:48 -0700

    Strings complete

--- helloworldtemplate.java
+++ helloworldtemplate.java
@@ -5,6 +5,7 @@ public class helloworldtemplate
         String string1 = "I am a string";
         String string2 = string1;
         String string3 = "I am also a string";
-        boolean stringsEqual = string1.compareTo(string3);
+        boolean stringsEqual;
+        stringsEqual = string1.equals(string3);
     }
 }
