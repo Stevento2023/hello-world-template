9aefb47e4c48e578b394ff4aecff918fc0cfccfb
Steven To <steven.to@mrjaffesclass.com>
2022-09-19 11:51:43 -0700

    Monday Array

--- helloworldtemplate.java
+++ helloworldtemplate.java
@@ -2,10 +2,17 @@ public class helloworldtemplate
 {
     public static void main(String[] args)
     {
-        String string1 = "I am a string";
-        String string2 = string1;
-        String string3 = "I am also a string";
-        boolean stringsEqual;
-        stringsEqual = string1.equals(string3);
+        String str0 = "Alpha";
+        String str1 = "Bravo";
+        String str2 = "Charlie";
+        String str3 = "Delta";
+        String str4 = "Echo";
+        String[] myArray = {str0, str1, str2, str3, str4};
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i]);
+        }
     }
 }
